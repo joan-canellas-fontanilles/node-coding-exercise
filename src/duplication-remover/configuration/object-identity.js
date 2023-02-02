745c67da8228ba0b0b4a5975755dbf1fa3c48e0b
Joan Cañellas Fontanilles <joan.canellas.fontanilles@gmail.com>
2023-02-02 10:16:32 +0100

    feat: Implement object identity to work with unordered properties

--- src/duplication-remover/configuration/object-identity.js
+++ src/duplication-remover/configuration/object-identity.js
@@ -1,5 +1,23 @@
 export class ObjectIdentity {
   generateIdentity(object) {
+    if (typeof object === "object") {
+      const objectEntries = this._getEntries(object);
+      object = Object.fromEntries(objectEntries);
+    }
     return JSON.stringify(object);
+  }
+
+  _getEntries(object) {
+    return Object.entries(object)
+      .filter(([propertyName, propertyValue]) =>
+        this._entryFilter(propertyName, propertyValue)
+      )
+      .sort(([a], [b]) => a.localeCompare(b));
+  }
+
+  _entryFilter(propertyName, propertyValue) {
+    return !(
+      typeof propertyValue === "object" || typeof propertyValue === "function"
+    );
   }
 }
